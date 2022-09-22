4c7bc02cd0f9d77eb458f3a5e264bbee8feca6d9
Claire List <claire.list2501@gmail.com>
2022-09-22 15:54:34 -0600

    error handling moved to same file

--- assignments/week-1/appending-users.sh
+++ assignments/week-1/appending-users.sh
@@ -3,6 +3,19 @@ echo Enter your FIRST name, LAST name, a valid EMAIL, and good PASSWORD \; seper
 read fname lname email password;
 
 args=$fname","$lname","$email","$password;
+
+string= grep -w -c $email mock-data.csv; #what I mean:: only search specific word, and count number
+#of times that word appears.
+
+
+if [ $string -gt 0]
+then
+echo 'same email detected!'
+else
+echo 'null same email'
+fi;
+
+
 echo $args >> mock-data.csv;
 
 cat mock-data.csv; #commented out be3cause it is giving me trouble...
--- assignments/week-1/check-email-unique.sh
+++ assignments/week-1/check-email-unique.sh
@@ -5,12 +5,30 @@
 #(handled, appending-users.sh)
 
 #step one: read through the contents of our data
-grep <expression to match> mock-data.csv
+#grep <expression to match> mock-data.csv
+
+### split on '@', only ahve to check first half
+## if firstHalfOfEmail = user-providedFirstHalfOfEmail,
+#we PROBABLY have a match.
+#this would of course not account for same usernames but different domains euargh!
+## But I think it is a reasonable assumption that someone with the same username but different
+### domains is PROABABLY the same person, right?
 
 #step two: use regex to check for matching email address
-if(match)
-reject;
+
+#if(match)
+#echo 'email matches an existing register, try again.';
+#exit;
 #on true match, reject user input
-elif(!match)
-next()
+#elif(!match)
+#next()
 #on !match, resume execution (where is next() when you need her???)
+
+string= cat mock-data.csv;
+substring= $email;
+if [[ $string =~ *"$substring"* ]];
+then
+echo 'same email detected!'
+else
+echo 'null same email'
+fi;

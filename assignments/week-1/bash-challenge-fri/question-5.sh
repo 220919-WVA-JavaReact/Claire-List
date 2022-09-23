21ce793985a9fb4c0556825feb5da28839ae9c95
Claire List <claire.list2501@gmail.com>
2022-09-23 14:51:10 -0600

    q5 logic sketch added to

---
+++ assignments/week-1/bash-challenge-fri/question-5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#given 10 numbers, determine how many are positive and negative (+self = 0 ?)
+# negcount++
+#10-negcount = poscount
+#echo the number of positive numbers is $poscount
+#echo the number of negative numbers is $negcount
+
+echo 'Enter 10 numbers!';
+read -a array n1 n2 n3 n4 n5 n6 n7 n8 n9 n0;
+
+poscount=0;
+negcount=0;
+
+for ((i=0; i<array.length; i++))
+do
+	
+done

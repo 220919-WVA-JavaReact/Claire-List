b0f42a2a1968cc7b84ccac2d5a8fd5dc8213c7e0
Claire List <claire.list2501@gmail.com>
2022-09-30 15:56:48 -0600

    added to todos will finish sat

---
+++ assignments/week-2/fri-java/calcul-actual/Java-calcul/src/main/java/com/revature/calc/DivisionEngine.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DivisionEngine {
+    public static void main(String[] args) {
+        boolean engine = true; //'engine' ie, running. Used in ancient times to refer to the mechanism (abstract) by which something is facilitated.
+
+        Scanner input = new Scanner(System.in);
+
+        while(engine){
+        //prompt user for input! EASY, right? ;)
+            System.out.println("")
+        }
+    }
+}
--- assignments/week-2/fri-java/calcul-actual/Java-calcul/src/main/java/com/revature/calc/Main.java
+++ assignments/week-2/fri-java/calcul-actual/Java-calcul/src/main/java/com/revature/calc/Main.java
@@ -3,6 +3,6 @@ package com.revature.calc;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("I yam a division engine application. I perform basic arethmetic functions, and display the results to stdout.");
     }
 }

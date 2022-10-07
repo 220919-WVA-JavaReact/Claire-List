2d8e47cc1d448908e17ff0a1e1814cc009e58074
Claire List <claire.list2501@gmail.com>
2022-10-07 15:41:30 -0600

    push

--- projects/testing-java/src/main/java/com/revature/ChallengeTest.java
+++ projects/testing-java/src/main/java/com/revature/ChallengeTest.java
@@ -1,2 +1,33 @@
-package com.revature;public class ChallengeTest {
+package com.revature;
+import org.junit.*;
+import com.revature.CodingChallenge;
+public class ChallengeTest {
+
+
+
+
+    public String str1 = "1";
+    public String str2 = "I am string 2";
+
+    static CodingChallenge chal;
+
+    //set up what should happen before each test...
+//    @BeforeClass
+//    public static void setUpBeforeClass(){
+//        System.out.println("This runs before the entire class but only once");
+//        chal = new CodingChallenge();
+//    }
+
+    @Test
+    public void returnsString(){
+        System.out.println("Returns a value of type string");
+        Assert.assertTrue(str1 != null); //
+    }
+
+    @Test
+    public void returnsExpectedValue(){
+        System.out.println("Returns the expected value.");
+
+    }
+
 }
--- projects/testing-java/src/main/java/com/revature/CodingChallenge.java
+++ projects/testing-java/src/main/java/com/revature/CodingChallenge.java
@@ -1,2 +1,16 @@
-package com.revature;public class CodingChallenge {
+package com.revature;
+
+public class CodingChallenge {
+    //Given a string, return the first vowel of that string.
+
+
+
+    public static void main(String[] args){
+    firstVowel fv = CodingChallenge.firstVowel("i am returned from the string");
+    }
+
+    public static String firstVowel(String ans){
+        ans = "answer";
+        return ans;
+    }
 }

c519075ac098f20a15dcae8855267994348ac16d
Claire List <claire.list2501@gmail.com>
2022-10-08 22:18:28 -0600

    tickets

--- projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAO.java
+++ projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAO.java
@@ -8,11 +8,14 @@ import java.util.List;
 import com.revature.models.Ticket;
 public class TicketDAO implements TicketDAOint {
     @Override
-    public Ticket createTicket(int created_by, String reason, float amount, String status) {
+    public Ticket createTicket(String reason, float amount, String status) {
         Ticket ticket = new Ticket();
 
         try (Connection conn = ConnectionUtil.getConnection()){
             String sql = "INSERT INTO tickets () VALUES (?,?,?,?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Unable to create the ticket, please try again.");
@@ -22,12 +25,12 @@ public class TicketDAO implements TicketDAOint {
     }
 
     @Override
-    public List<Ticket> getUserTickets() {
+    public List<Ticket> getUserTickets(String user_name) {
         return null;
     }
 
     @Override
-    public List<Ticket> getAllTickets() {
+    public List<Ticket> getAllTickets(String user_name) {
         return null;
     }
 }
--- projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAOint.java
+++ projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAOint.java
@@ -5,9 +5,11 @@ import com.revature.models.Ticket;
 import java.util.List;
 
 public interface TicketDAOint {
-    Ticket createTicket(int created_by, String reason, float amount, String status);
+    Ticket createTicket(String reason, float amount, String status);
 
-    List<Ticket> getUserTickets(); //"I am an employee, I can only see MY tickets"
+    List<Ticket> getUserTickets(String user_name); //"I am an employee, I can only see MY tickets". If user_name = user.getUser_name() , allow
+                                                                                                    // else, disallow
 
-    List<Ticket> getAllTickets(); //"I am a manager, I can see all tickets"
+    List<Ticket> getAllTickets(String user_name); //"I am a manager, I can see all tickets". Do not check if this username == own user username, uneeded.
+                                                                    //THIS username is used to print out that specific user's tickets
 }

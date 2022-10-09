4f2a197f9ed5a1b8e8f4bf770503dc60a1d1e98b
Claire List <claire.list2501@gmail.com>
2022-10-09 10:03:47 -0600

    hacking out ticket

--- projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAO.java
+++ projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAO.java
@@ -1,6 +1,7 @@
 package com.revature.DAO;
 import com.revature.util.ConnectionUtil;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 //call getByUsername, extract the user id, feed that to created_by
 //also feed this user's user_name to getAllTickets() (--> manager's thing) !
@@ -30,7 +31,26 @@ public class TicketDAO implements TicketDAOint {
     }
 
     @Override
-    public List<Ticket> getAllTickets(String user_name) {
-        return null;
+    public List<Ticket> getAllTickets(int user_id){ //"I am a manager, I can see all tickets". Do not check if this username == own user username, uneeded.
+        //THIS username is used to print out that specific user's tickets
+        Connection conn = ConnectionUtil.getConnection();
+        List<Ticket> tickets = new ArrayList<>();
+
+        try{
+
+
+            String usSQL = "SELECT users.user_name, tickets.ticket_id, tickets.reason, tickets.amount, tickets.status FROM tickets LEFT JOIN users ON users.? = tickets.created_by;"; //"all except created_by int is needed, bc we have th euser name.
+            PreparedStatement stmt = conn.prepareStatement(usSQL);
+
+            stmt.setInt(1, user_id);
+            ResultSet rs = stmt.executeQuery(usSQL);
+
+            while (rs.next()){
+                Ticket tickets = new Ticket(ticket_id, createdBy, reason, amount, status)
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return tickets;
     }
 }
--- projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAOint.java
+++ projects/foundation-redux/src/main/java/com/revature/DAO/TicketDAOint.java
@@ -13,22 +13,5 @@ public interface TicketDAOint {
     List<Ticket> getUserTickets(String user_name); //"I am an employee, I can only see MY tickets". If user_name = user.getUser_name() , allow
                                                                                                     // else, disallow
 
-    List<Ticket> getAllTickets(String user_name){ //"I am a manager, I can see all tickets". Do not check if this username == own user username, uneeded.
-        //THIS username is used to print out that specific user's tickets
-        Connection conn = ConnectionUtil.getConnection();
-        List<Ticket> tickets = new ArrayList<>();
-
-        try{
-            Statement stmt = conn.createStatement();
-
-            String usSQL = "SELECT tickets.*, users.user_name FROM tickets LEFT JOIN users ON users.user_id = tickets.created_by;";
-            ResultSet rs = stmt.executeQuery(usSQL);
-
-            while (rs.next()){
-
-            }
-        } catch (SQLException e){
-            e.printStackTrace();;
-        }
-    }
+    List<Ticket> getAllTickets(int user_id);
 }

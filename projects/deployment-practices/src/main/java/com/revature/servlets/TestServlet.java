915c0e8f07fb11a60ecbacfb61ec4102bd23fa9d
Claire List <claire.list2501@gmail.com>
2022-10-15 10:30:34 -0600

    servlet insanity

--- projects/deployment-practices/src/main/java/com/revature/servlets/TestServlet.java
+++ projects/deployment-practices/src/main/java/com/revature/servlets/TestServlet.java
@@ -12,19 +12,19 @@ import java.time.LocalDateTime;
 //BE SURE TO CREATE THE LISTENER CONTEXT !
 public class TestServlet extends HttpServlet {
 
-    @Override
-    public void init() throws ServletException {
-        System.out.println("[LOG] - SanityServlet Instantiated!");
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("[LOG] - SanityServlet Instantiated!");
+//
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("[LOG] - SanityServlet Destroyed");
+//    }
 
-    }
-
     @Override
-    public void destroy() {
-        System.out.println("[LOG] - SanityServlet Destroyed");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { //removed ServletException
 
         // We'll just some println statements to check some information about the request
         System.out.println("[LOG] - SanityServlet received a request at " + LocalDateTime.now());
@@ -33,8 +33,10 @@ public class TestServlet extends HttpServlet {
         System.out.println("[LOG] - Request Header, example: " + req.getHeader("example"));
         System.out.println("[LOG] - Request Query String: " + req.getQueryString());
 
-//        System.out.println("[LOG] - was filtered? " + req.getAttribute("was-filtered"));
+        System.out.println("[LOG] - was filtered? " + req.getAttribute("was-filtered"));
         //if you want to do custom filtering, will have to build that
+
+        System.out.println("[LOG] - was filtered? " + req.getAttribute("was-filtered"));
 
         // We'll formulate a basic response to send back
         // Set the status code
--- projects/deployment-practices/src/main/java/com/revature/util/ContextListener.java
+++ projects/deployment-practices/src/main/java/com/revature/util/ContextListener.java
@@ -1,48 +1,48 @@
-package com.revature.util;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.ServletRegistration;
-import java.time.LocalDateTime;
-
-import com.revature.servlets.TestServlet; //I like to import external libs on top, own packages next
-
-public class ContextListener implements ServletContextListener { //I WAS LITERALLY WORKING UNTIL I ADDED THIS CONTEXT LISTENER!
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("[LOG] - The servlet context was initialized at " + LocalDateTime.now());
-
-        // We can also programmatically define/register our servlets with the container here
-
-        //ObjectMapper mapper = new ObjectMapper();
-
-        TestServlet testServlet = new TestServlet();
-
-
-        // Now I need to register it with the context itself
-        ServletContext context = sce.getServletContext();
-        ServletRegistration.Dynamic registeredServlet = context.addServlet("TestServlet", testServlet);
-        // Now I can affect the fields that I wanted to before
-        registeredServlet.addMapping("/tests");
-        registeredServlet.setLoadOnStartup(3);
-        registeredServlet.setInitParameter("test-servlet-key", "test-servlet-value");
-        registeredServlet.setInitParameter("another-param", "another-value");
-
-        /*
-        ExampleServlet exampleServlet = new ExampleServlet(mapper);
-        context.addServlet("ExampleServlet", exampleServlet).addMapping("/examples");
-         */
-
-        // Register the servlet
-//        context.addServlet("FlashcardServlet", flashcardServlet).addMapping("/flashcards/*");
-//        context.addServlet("AuthServlet", authServlet).addMapping("/auth");
-        context.addServlet("TestServlet", testServlet).addMapping("/test");
-    }
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("[LOG] - The servlet context was destroyed at " + LocalDateTime.now());
-    }
-}
+//package com.revature.util;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletContextEvent;
+//import javax.servlet.ServletContextListener;
+//import javax.servlet.ServletRegistration;
+//import java.time.LocalDateTime;
+//
+//import com.revature.servlets.TestServlet; //I like to import external libs on top, own packages next
+//
+//public class ContextListener implements ServletContextListener { //I WAS LITERALLY WORKING UNTIL I ADDED THIS CONTEXT LISTENER!
+//    @Override
+//    public void contextInitialized(ServletContextEvent sce) {
+//        System.out.println("[LOG] - The servlet context was initialized at " + LocalDateTime.now());
+//
+//        // We can also programmatically define/register our servlets with the container here
+//
+//        //ObjectMapper mapper = new ObjectMapper();
+//
+//      //  TestServlet testServlet = new TestServlet();
+//
+//
+//        // Now I need to register it with the context itself
+//        ServletContext context = sce.getServletContext();
+//        ServletRegistration.Dynamic registeredServlet = context.addServlet("TestServlet", testServlet);
+//        // Now I can affect the fields that I wanted to before
+//        registeredServlet.addMapping("/tests");
+//        registeredServlet.setLoadOnStartup(3);
+//        registeredServlet.setInitParameter("test-servlet-key", "test-servlet-value");
+//        registeredServlet.setInitParameter("another-param", "another-value");
+//
+//        /*
+//        ExampleServlet exampleServlet = new ExampleServlet(mapper);
+//        context.addServlet("ExampleServlet", exampleServlet).addMapping("/examples");
+//         */
+//
+//        // Register the servlet
+////        context.addServlet("FlashcardServlet", flashcardServlet).addMapping("/flashcards/*");
+////        context.addServlet("AuthServlet", authServlet).addMapping("/auth");
+//        context.addServlet("TestServlet", testServlet).addMapping("/test");
+//    }
+//
+//    @Override
+//    public void contextDestroyed(ServletContextEvent sce) {
+//        System.out.println("[LOG] - The servlet context was destroyed at " + LocalDateTime.now());
+//    }
+//}
